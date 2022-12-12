dfb6bc3a7cbbd89a11c1b6447b3e7c1a8ac79c9c
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-13 00:42:17 +0600

    font family, react-bootstrap, bootstrap installed

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
     return (
         <div>
-            This is Home Page
+            <Button>Hello</Button>
         </div>
     );
 };
