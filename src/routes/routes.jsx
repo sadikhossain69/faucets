1a1edb53c1b164846e3399f64bd94e51fd3438f7
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-13 00:33:21 +0600

    initial

---
+++ src/routes/routes.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom"
+import Home from "../layout/Home"
+import Login from "../pages/Login"
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/login',
+        element: <Login />
+    }
+])
+
+export default routes
