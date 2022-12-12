12cfe4e1d1869ac350f7e7a970f556c6be4a05e3
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-13 04:18:15 +0600

    FormSection created

---
+++ src/pages/MainSection/FormSection/FormSection.jsx
@@ -0,0 +1,11 @@
+import "./FormSection.css"
+
+const FormSection = () => {
+    return (
+        <div style={{backgroundColor: "#FFFFFF", padding: "15px"}}>
+            This is Form Section
+        </div>
+    );
+};
+
+export default FormSection;
--- src/pages/MainSection/MainSection.jsx
+++ src/pages/MainSection/MainSection.jsx
@@ -1,9 +1,11 @@
+import FormSection from "./FormSection/FormSection";
 import Request from "./Request/Request";
 
 const MainSection = () => {
     return (
         <div style={{ backgroundColor: "#EEF2FE", padding: "30px" }}>
             <Request />
+            <FormSection />
         </div>
     );
 };
