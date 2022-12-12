4ece3b429993a884b57c2a885189dae54678650d
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-13 02:31:55 +0600

    Navbar done

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Navbar from './Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
-            <Button>Hello</Button>
+            <Navbar />
         </div>
     );
 };
---
+++ src/layout/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Button, Container, Dropdown, DropdownButton, NavbarBrand, Stack } from "react-bootstrap";
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <Stack direction="horizontal" className="justify-content-between p-3" gap={3}>
+            <div className="fs-3 fw-bold navTitle">Faucets</div>
+            <div className="d-flex justify-content-center">
+                <select className="bg-light border px-4 py-2">
+                    <option selected >Ethereum Kovan</option>
+                    <option >Arbitrum Rinkeby</option>
+                    <option >BNB Chain Testnet</option>
+                    <option >Ethereum Rinkeby</option>
+                    <option >Fantom Testnet</option>
+                    <option >Harmony Testnet</option>
+                    <option >POA Network Sokol</option>
+                    <option >Polygon Mumbai</option>
+                    <option >Harmony Testnet</option>
+                </select>
+                <div className="bg-light px-4 py-2 fw-bold navTitle navbarBtn mx-3">Connect Wallet</div>
+                <i className="fa-regular fa-user fontSize p-2"></i>
+            </div>
+        </Stack>
+
+    );
+};
+
+export default Navbar;
