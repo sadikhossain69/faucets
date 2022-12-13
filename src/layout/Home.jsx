0e12df0a8b5f631425c693a4ba7ffa373baec457
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-13 07:36:35 +0600

    navbar editing done

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -8,7 +8,7 @@ import Navbar from './Navbar/Navbar';
 const Home = () => {
     return (
         <div>
-            {/* <Navbar /> */}
+            <Navbar />
             <NoticeHere />
             <MainSection />
             <Footer />
--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -4,24 +4,24 @@ import "./Navbar.css"
 const Navbar = () => {
     return (
         <div>
-            <Stack direction="horizontal" className="justify-content-between p-3" gap={3}>
-            <div className="fs-3 fw-bold navTitle">Faucets</div>
-            <div className="d-flex justify-content-center">
-                <select className="bg-light border px-4 py-2">
-                    <option defaultValue={"Ethereum Kovan"} >Ethereum Kovan</option>
-                    <option >Arbitrum Rinkeby</option>
-                    <option >BNB Chain Testnet</option>
-                    <option >Ethereum Rinkeby</option>
-                    <option >Fantom Testnet</option>
-                    <option >Harmony Testnet</option>
-                    <option >POA Network Sokol</option>
-                    <option >Polygon Mumbai</option>
-                    <option >Harmony Testnet</option>
-                </select>
-                <div className="bg-light px-4 py-2 fw-bold navTitle navbarBtn mx-3">Connect Wallet</div>
-                <i className="fa-regular fa-user fontSize p-2"></i>
+            <div className="nav-container" gap={3}>
+                <div className="fs-3 fw-bold navTitle">Faucets</div>
+                <div className="d-flex option-nav">
+                    <select className="bg-light border px-4 py-2">
+                        <option defaultValue={"Ethereum Kovan"} >Ethereum Kovan</option>
+                        <option >Arbitrum Rinkeby</option>
+                        <option >BNB Chain Testnet</option>
+                        <option >Ethereum Rinkeby</option>
+                        <option >Fantom Testnet</option>
+                        <option >Harmony Testnet</option>
+                        <option >POA Network Sokol</option>
+                        <option >Polygon Mumbai</option>
+                        <option >Harmony Testnet</option>
+                    </select>
+                    <div className="bg-light px-4 py-2 fw-bold navTitle navbarBtn mx-3">Connect Wallet</div>
+                    <i className="fa-regular fa-user fontSize p-2"></i>
+                </div>
             </div>
-        </Stack>
         </div>
 
     );
