d8df541c016cb305b9bfe7afb15464dfc5731ffd
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-13 09:29:05 +0600

    sign up massage done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,31 @@
-import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { GoogleLogin, } from '@react-oauth/google';
 import './Login.css'
 import jwt_decode from "jwt-decode";
 import { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import cogoToast from 'cogo-toast';
 
 const Login = () => {
 
     const [auth, setAuth] = useState({})
+    const navigate = useNavigate()
 
     return (
         <div className='authDesign'>
             <GoogleLogin
                 onSuccess={credentialResponse => {
                     let decoded = jwt_decode(credentialResponse.credential);
+                    cogoToast.success(`You have successfully login by ${decoded.email}`)
                     setAuth(decoded);
+                    navigate('/')
                 }}
                 onError={() => {
                     console.log('Login Failed');
                 }}
             />
             {
-                auth && <Button style={{ marginLeft: "10px" }} onClick={() => googleLogout()}>Log Out</Button>
+                auth && <Button style={{ marginLeft: "10px" }}>Log Out</Button>
             }
         </div>
     );
